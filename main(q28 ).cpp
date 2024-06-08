9cf963378c954fe4e629b4ae71c98d36bd0fe9dc
hengamehsh59 <hengamehsh59@gmail.com>
2024-06-08 18:53:52 +0330

    Add files via upload

---
+++ main(q28 ).cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int hour;
+    cin>>hour;
+    if(hour>12){
+        cout<<"PM";
+    }
+    else{
+        cout<<"AM";
+    }
+    return 0;
+}
